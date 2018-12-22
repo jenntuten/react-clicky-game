d8c5f7cd3e3ca9c2e31236fd346529a4c4a9918c
jenn210 <jennifer.c.tuten@gmail.com>
2018-12-22 01:21:32 -0600

    Updates

---
+++ precache-manifest.28ae729adb1682b66c77f99a28b8e3ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "e266201ae218053999aa",
+    "url": "/react-clicky-game/static/js/main.e266201a.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/react-clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "e266201ae218053999aa",
+    "url": "/react-clicky-game/static/css/main.516645de.chunk.css"
+  },
+  {
+    "revision": "eba0cffb5a869f4ed0b1151407e21eb1",
+    "url": "/react-clicky-game/index.html"
+  }
+];
